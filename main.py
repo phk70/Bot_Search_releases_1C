3f9641763d78ac937f493e7a2513a06012361016
koles <kolesnikov.pavel.sergeevich@gmail.com>
2024-12-17 23:36:16 +0700

    Доделана проверка новых версий. Бот работает. Доделать изменение текущей версии.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from turtle import up
 import lxml
 import time
 import os
+import json
 
 from pprint import pprint
 from bs4 import BeautifulSoup
@@ -11,19 +13,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-LIST_RELEASES=[]
-
+VERSION="1.3.235.2"
 
 def login(url, username, password):
-    print("[+]...Открываю браузер...")
-    print(url, username, password)
+    print("[+]...Открываю браузер...")    
     driver = webdriver.Firefox()
 # Входим на сайт
     driver.get(url)
 # Находим и заполняем поле Username
-    print("[+]...Ввожу Username...")
-    time.sleep(4)
+    print("[+]...Ввожу Username...")    
     username_form = driver.find_element(By.ID, "username")     
     username_form.send_keys(username)  
 # Находим и заполняем поле Password        
@@ -41,12 +39,12 @@ def login(url, username, password):
         file.write(html)
     print("[+]...Веб страница сохранена...")
 # Закрываем браузер
-    time.sleep(4)
     print("[+]...Закрываю браузер...")
     driver.close()
     
 
 def search_all_versions(file_name):
+    list_releases=[]
 # Открываем html в переменную data
     with open(file_name, 'r', encoding="utf-8") as file:
         data = file.read()
@@ -58,37 +56,66 @@ def search_all_versions(file_name):
     row_list = table.find_all('tr')[1:]
 # Вытаскиваем из каждой строки колонку и вставляем в список
     id = 0
+    
     for row in row_list:
         td_list = row.find_all("td")
-# Записываем из колонки с версиями
+# Записываем из колонки с версиями       
         number_version = td_list[0].find('a').text.strip()
+# Записываем из колонки с датой выхода
+        date_release = td_list[1].text.strip()
 # Записываем из колонки с версиями для обновлений   
         update_version = td_list[2].text.strip()
 # Добавляем все в список
-        LIST_RELEASES.append({
+        list_releases.append({
             'id': id,
             'number_version': number_version,
+            'date_release': date_release,
             'update_version': update_version,
         })
         id += 1
+# И сохраняем все в json
+
+        with open('list_releases.json', 'w', encoding="utf-8") as file:
+            json.dump(list_releases, file, indent=4, ensure_ascii=False)
+    
         
 
-def serch_my_version(LIST_RELEASES, version):         
-    count=0
+def serch_up_for_my_version(list_releases, version):       
+# Открываем файл на чтение
+    with open(list_releases, 'r', encoding="utf-8") as file:
+        data = json.load(file)  
+# Проходимся по каждому элемента json для сравнения версий и выводим количество версий для обновлени
+    count = 0
+    new_ver = ''
+    for ver in data:    
+# Проверяем совпадение текущей версии и списка новых версий   
+        try:
+            if (ver['number_version'].split('.')[2] != version.split('.')[2]) and (version not in ver['update_version']):
+                new_ver = f'Новая версия == {ver['number_version']} от {ver["date_release"]}'           
+        except:
+            pass
+# Проверяем совпадение текущей версии и списка версий для обновлений
+        try:    
+            if version in ver['update_version']:
+                count += 1
+        except:
+            pass        
+# Если доходим до текущей версии прекращам цикл
+        if ver['number_version'] == version:
+                break
+# Возвращаем сообщение
+    message_version = f'База "Управление Торговлей 1.3\n\nТвоя версия == {version}\nДоступно обновлений этой версии == {count}\n{new_ver}\n\nНе мучай себя и коллег...\nПожалуйста обнови версию...'
+    return message_version
 
-    for ver in LIST_RELEASES: 
-        count += 1 
-        if version in ver['update_version']:    
-            print(f'Вышло обновление {ver['number_version']}.')
-            break  
+def update_version(new_version):
+    global VERSION
+    VERSION = new_version
     
-    return f'Доступно обновлений для текущей версии: {count}'
-
 
 def main():
     login(os.getenv('URL'), os.getenv('LOGIN'), os.getenv('PASSWORD'))    
     search_all_versions('list_base.html')
-    serch_my_version(LIST_RELEASES, os.getenv('VERSION'))
+    serch_up_for_my_version('list_releases.json', VERSION)   
 
 
 if __name__ == "__main__":
--- send_message_telegram.py
+++ send_message_telegram.py
@@ -2,7 +2,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
 
-from main import *
+from main import login, search_all_versions, serch_up_for_my_version, update_version, VERSION
 
 import os
 
@@ -10,26 +10,31 @@ import os
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
-
+# Стартовое меню
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    start_buttons = ['Текущая версия', 'Проверить обновление']
+    start_buttons = ['Текущая версия', 'Проверить обновление', 'Обновить версию']
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*start_buttons)
     await message.answer('Выберите действие', reply_markup=keyboard)
-
     
-
+# Нажатие на кнопку Текущая версия
 @dp.message_handler(Text(equals='Текущая версия'))
 async def send_version(message: types.Message):
-    await message.answer(f'Текущая версия: {os.getenv("VERSION")}')
+    await message.answer(f'Текущая версия: {VERSION}')
 
-    
+# Нажатие на кнопку Проверить обновление
 @dp.message_handler(Text(equals='Проверить обновление'))
 async def send_version(message: types.Message):
+    login(os.getenv('URL'), os.getenv('LOGIN'), os.getenv('PASSWORD'))   
     search_all_versions('list_base.html')
-    await message.answer(serch_my_version(LIST_RELEASES, os.getenv('VERSION')))
-    
+    await message.answer(serch_up_for_my_version('list_releases.json', VERSION))
+
+# Нажатие на кнопку Обновить версию
+@dp.message_handler(Text(equals='Обновить версию'))
+async def update_version_in_sistem(message: types.Message):
+    await message.answer('В разработке...')
+   
     
 def main():
     executor.start_polling(dp)
