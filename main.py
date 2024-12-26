325b7d6e84e17f80d62e66bfc060430821169548
koles <kolesnikov.pavel.sergeevich@gmail.com>
2024-12-26 14:16:45 +0700

    доделать циклический запуск и обновление версии

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-from turtle import up
 import lxml
 import time
 import os
 import json
 
-from pprint import pprint
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
+from threading import Timer
 
 load_dotenv()
 
-VERSION=["1.3.235.2"]
 
-def original_version(version):
-    return version[0]
+# Просмотр версии
+def view_version():    
+    with open("version.json", 'r', encoding="utf-8") as file:
+        version = json.load(file)    
+    return version['version']
+    
+    
+        
+# Запись версии в файл
+def save_version(version):
+    actual_version = {"version": version}
+    with open("version.json", 'w', encoding="utf-8") as file:
+        json.dump(actual_version, file, indent=4, ensure_ascii=False)
+    print(f"[+]...Версия {version} сохранена...")
 
-
+# Вход на сервис 1С
 def login(url, username, password):
     print("[+]...Открываю браузер...")    
     driver = webdriver.Firefox()
@@ -34,10 +44,10 @@ def login(url, username, password):
     password_form.send_keys(password)
 # Входим
     print("[+]...Вхожу на сайт...")
-    time.sleep(1)
+    time.sleep(2)
     password_form.send_keys(Keys.RETURN)    
 # Сохраняем страницу
-    time.sleep(4)
+    time.sleep(6)
     html = driver.page_source
     with open("list_base.html", 'w', encoding="utf-8") as file:
         file.write(html)
@@ -47,20 +57,19 @@ def login(url, username, password):
     driver.close()
     
 
+# Поиск всех версий и запись их в файл
 def search_all_versions(file_name):
     list_releases=[]
 # Открываем html в переменную data
     with open(file_name, 'r', encoding="utf-8") as file:
         data = file.read()
 # Запихиваем ее в BS4    
-    soup = BeautifulSoup(data, 'lxml')
+    soup = BeautifulSoup(data, "lxml")
 # Вытаскиваем таблицу из html
-    table = soup.find('table', class_='customTable table-hover')
+    table = soup.find("table", attrs={'class': 'customTable table-hover'}) 
 # Вытаскиваем все строки (tr) без заголовка
-    row_list = table.find_all('tr')[1:]
-# Вытаскиваем из каждой строки колонку и вставляем в список
-    id = 0
-    
+    row_list = table.find_all("tr")[1:]
+# Вытаскиваем из каждой строки колонку и вставляем в список     
     for row in row_list:
         td_list = row.find_all("td")
 # Записываем из колонки с версиями       
@@ -70,22 +79,22 @@ def search_all_versions(file_name):
 # Записываем из колонки с версиями для обновлений   
         update_version = td_list[2].text.strip()
 # Добавляем все в список
-        list_releases.append({
-            'id': id,
+        list_releases.append({            
             'number_version': number_version,
             'date_release': date_release,
             'update_version': update_version,
-        })
-        id += 1
+        })        
 # И сохраняем все в json
-
         with open('list_releases.json', 'w', encoding="utf-8") as file:
             json.dump(list_releases, file, indent=4, ensure_ascii=False)
-    
-        
+    print("[+]...Все версии сохранены...")        
 
-def serch_up_for_my_version(list_releases, version):       
-# Открываем файл на чтение
+
+# Поиск обновлений для моей версии
+def serch_up_for_my_version(list_releases):     
+# Добываем текущую версию  
+    version = view_version()
+# Открываем файл со всеми версиями на чтение
     with open(list_releases, 'r', encoding="utf-8") as file:
         data = json.load(file)  
 # Проходимся по каждому элемента json для сравнения версий и выводим количество версий для обновлени
@@ -94,33 +103,29 @@ def serch_up_for_my_version(list_releases, version):
     for ver in data:    
 # Проверяем совпадение текущей версии и списка новых версий   
         try:
-            if (ver['number_version'].split('.')[2] != version[0].split('.')[2]) and (version[0] not in ver['update_version']):
+            if (ver['number_version'].split('.')[2] != version.split('.')[2]) and (version not in ver['update_version']):
                 new_ver = f'Новая версия == {ver['number_version']} от {ver["date_release"]}'           
         except:
             pass
 # Проверяем совпадение текущей версии и списка версий для обновлений
         try:    
-            if version[0] in ver['update_version']:
+            if version in ver['update_version']:
                 count += 1
         except:
             pass        
 # Если доходим до текущей версии прекращам цикл
-        if ver['number_version'] == version[0]:
+        if ver['number_version'] == version:
                 break
 # Возвращаем сообщение
-    message_version = f'База "Управление Торговлей 1.3\n\nТвоя версия == {version[0]}\nДоступно обновлений этой версии == {count}\n{new_ver}\n\nНе мучай себя и коллег...\nПожалуйста обнови версию...'
+    message_version = f'База "УПП" 1.3\n\nТвоя версия == {version}\nДоступно обновлений этой версии == {count}\n{new_ver}\n\nНе мучай себя и коллег...\nПожалуйста обнови версию...'
     return message_version
 
-def update_version(new_version):
-    global VERSION
-    VERSION[0] = new_version
-    
-
-# def main():
-#     login(os.getenv('URL'), os.getenv('LOGIN'), os.getenv('PASSWORD'))    
-#     search_all_versions('list_base.html')
-#     serch_up_for_my_version('list_releases.json', VERSION[0])   
 
 
-if __name__ == "__main__":
-    main()
+# ТЕСТЫ
+# login(os.getenv('URL'), os.getenv('LOGIN'), os.getenv('PASSWORD'))    
+# search_all_versions('list_base.html')
+# print(serch_up_for_my_version('list_releases.json'))   
+# save_version('1.3.236.1')
+# print(view_version())
+
--- tg_bot.py
+++ tg_bot.py
@@ -1,60 +1,66 @@
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters import Text
-
-
-from main import login, original_version, search_all_versions, serch_up_for_my_version, update_version, VERSION
+from dotenv import load_dotenv
+from main import login, view_version, save_version, search_all_versions, serch_up_for_my_version
+import time
 
 import os
+load_dotenv()
 
 
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
+
 # Стартовое меню
 @dp.message_handler(commands='start')
-async def start(message: types.Message):
+async def start(message: types.Message): 
     start_buttons = ['Текущая версия', 'Проверить обновление', 'Обновить версию']
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="Что делаем?")
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="")
     keyboard.add(*start_buttons)
     await message.answer('Выберите действие', reply_markup=keyboard)
-    
+
+
 # Нажатие на кнопку Текущая версия
 @dp.message_handler(Text(equals='Текущая версия'))
 async def send_version(message: types.Message):    
-    await message.answer(f'Текущая версия: {original_version(VERSION)}')
+    await message.answer(f'Текущая версия: {view_version()}')
+
 
 # Нажатие на кнопку Проверить обновление
 @dp.message_handler(Text(equals='Проверить обновление'))
 async def search_updates(message: types.Message):
     login(os.getenv('URL'), os.getenv('LOGIN'), os.getenv('PASSWORD'))   
     search_all_versions('list_base.html')
-    await message.answer(serch_up_for_my_version('list_releases.json', VERSION))
+    await message.answer(serch_up_for_my_version('list_releases.json'))
+
 
 # Нажатие на кнопку Обновить версию
 @dp.message_handler(Text(equals='Обновить версию'))
 async def update_version_in_sistem(message: types.Message): 
-    up_kb = ["Обновить", "Назад"]
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="Что делаем?")
+    up_kb = ["Назад"]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="")
     keyboard.add(*up_kb)
     await message.answer('Введите новую версию в формате X.X.XXX.X', reply_markup=keyboard)
 
-# Нажатие на кнопку Обновить
-@dp.message_handler(Text(equals='Обновить'))
-async def up_version(message: types.Message):   
-    update_version(message.text)        
-    await message.answer(f'Версия обновлена. Текущая версия: {original_version(VERSION)}')
-        
+
+@dp.message_handler()
+async def update_version_in_sistem(message: types.Message):         
+    new_version = message.text
+    save_version(new_version)
+    await message.answer('Версия обновлена')
+
+
 # Нажатие на кнопку Назад
 @dp.message_handler(Text(equals='Назад'))
-async def up_version(message: types.Message):    
-    if 'Назад' in message.text:
-        start_buttons = ['Текущая версия', 'Проверить обновление', 'Обновить версию']
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="Что делаем?")
-        keyboard.add(*start_buttons)
-        await message.answer('Выберите действие', reply_markup=keyboard)
-   
-    
+async def back(message: types.Message): 
+    start_buttons = ['Текущая версия', 'Проверить обновление', 'Обновить версию']
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder="")
+    keyboard.add(*start_buttons)
+    await message.answer('Выберите действие', reply_markup=keyboard)
+
+
 def main():
     executor.start_polling(dp)
 
